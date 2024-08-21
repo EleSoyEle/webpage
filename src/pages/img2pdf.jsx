541e5e0631d0dd071be4166a7078102a9b68c5ed
EleSoyEle <angelgvsgm@gmail.com>
2024-08-20 22:13:10 -0600

    mas codigo

--- src/pages/img2pdf.jsx
+++ src/pages/img2pdf.jsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
-import React from "react";
+import {React, useState} from "react";
 import { Route, useNavigate, Routes } from 'react-router-dom';
-
+import jsPDF from 'jspdf';
 import Table from './Table';
 
 const Img2pdf = () =>{
@@ -10,6 +10,42 @@ const Img2pdf = () =>{
     function ToInitMenu(){
         navigate("/")
     }
+    
+    const [image, setImage] = useState(null);
+    const [imgd,setImgdata] = useState(null);
+    const [fnm,setFname] = useState(null);
+    const uploadFile = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const name = file.name;
+            const fname = name.split(".")[0];
+            // Convertir imagen a base64
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const imgData = e.target.result;
+                setImgdata(imgData);
+                setFname(fname)
+                //makePDF(imgData, fname);
+          };
+            reader.readAsDataURL(file);
+        } 
+        else {
+            alert('Please select a file first.');
+        }
+    };
+
+    function downloadPDF(){
+        makePDF(imgd,fnm);
+    }
+
+
+    const makePDF = (imgData, name) => {
+        const doc = new jsPDF();
+        doc.addImage(imgData, 'JPEG', 10, 10, 180, 160); // Ajusta según sea necesario
+        //doc.output('dataurlnewwindow'); // Abre el PDF en una nueva ventana
+        doc.save(`${name}.pdf`); 
+};
+    
     return(
         <section>
             <div className='App-header'>
@@ -22,6 +58,7 @@ const Img2pdf = () =>{
                             </button>
                             <button className='btn btn-outline-primary'>
                                 Convertir imagenes a pdf
+                                
                             </button>
                             <button className='btn btn-primary'>
                                 Convertir .vid a mp4
@@ -30,12 +67,21 @@ const Img2pdf = () =>{
                         <div className='col'>
                             <p className='lead'>
                                 Aqui puedes subir tu archivo en formato png, jpg, jpeg</p>
-                            <button className='btn btn-primary'>Subir archvio</button>
+                        
+                            <input id="file" 
+                            type="file" 
+                            name="static_file" 
+                            accept='image/*'
+                            
+                            className='btn btn-primary' onChange={uploadFile}/>
+
+                            <button onClick={() => {if(imgd) downloadPDF();}} 
+                            className='btn btn-primary'> Upload </button>
                         </div>
                         <div className='col'></div>
                     </div>
-                </div>               
-            </div>
+                </div>
+            </div>               
             <Routes>
                 <Route
                     path="/"
